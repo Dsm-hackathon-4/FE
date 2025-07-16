4d1e55d3baf64fb5ddc669fa53783af0cf93928a
seungW0o <ssounsowns@gmail.com>
2025-07-17 00:17:50 +0900

    토스트 ui 세팅

--- src/App.tsx
+++ src/App.tsx
@@ -2,12 +2,14 @@ import { RouterProvider } from "react-router-dom";
 import router from "@/router";
 import { ThemeProvider } from "@emotion/react";
 import { GlobalStyles, theme } from "@/themes";
+import { Toaster } from 'react-hot-toast';
 
 function App() {
   return (
     <ThemeProvider theme={theme}>
       <GlobalStyles />
       <RouterProvider router={router} />
+      <Toaster />
     </ThemeProvider>
   );
 }
